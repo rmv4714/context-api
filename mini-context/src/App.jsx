6f11fc78f0dbaf18c5e33c14aeabb72e4714697d
rmv4714 <shwetakamal4714@gmail.com>
2023-10-04 15:47:13 +0530

    use Context

--- mini-context/src/App.jsx
+++ mini-context/src/App.jsx
@@ -1,15 +1,16 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import UserContextProvider from "./context/UserContextProvider";
 import "./App.css";
+import Login from "./components/Login";
+import Profile from "./components/Profile";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <>
+    <UserContextProvider>
       <h1>Hello</h1>
-    </>
+      <Login />
+      <Profile />
+    </UserContextProvider>
   );
 }
 
---
+++ mini-context/src/context/UserContext.js
@@ -0,0 +1,8 @@
+// pure js not jsx
+
+import React from "react";
+
+// 1. to create context
+const UserContext = React.createContext();
+
+export default UserContext;
